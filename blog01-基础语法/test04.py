af903b23ce2b45017ace4504d0d52da73110fe0e
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-09-09 11:26:33 +0800

    Add files via upload

---
+++ blog01-基础语法/test04.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'runoob','code':6734, 'dept': 'sales'}
+ 
+print(dict['one']) #输出键为one的值
+print(dict[2])     #输出键为2的值
+print(tinydict)    #输出完整字典
+print(tinydict.keys())   #输出所有键
+print(tinydict.values()) #输出所有值
