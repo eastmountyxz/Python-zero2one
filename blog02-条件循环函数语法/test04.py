0601c0c1619c8eab89a55ca405c6a7135af76b68
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-09-10 18:57:21 +0800

    Add files via upload

---
+++ blog02-条件循环函数语法/test04.py
@@ -0,0 +1,15 @@
+# 判断值是否在小于0或大于10
+num = 10
+if num < 0 or num > 10:    
+    print('hello')
+else:
+    print('undefine')
+# 输出结果: undefine
+
+
+# 判断值是否在0~5或者10~15之间
+num = 8
+if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):    
+    print('hello')
+else:
+    print('undefine')
