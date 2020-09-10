29b0a9206ac21222b80bbfbf96afd6d8cc8827be
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-09-10 18:56:53 +0800

    Add files via upload

---
+++ blog02-条件循环函数语法/test03.py
@@ -0,0 +1,13 @@
+num = input("please input:")
+num = int(num)
+print(num)
+if num >= 90:
+    print('A Class')
+elif num >= 80:
+    print('B Class')
+elif num >= 70:
+    print('C Class')
+elif num >=60:
+    print('D Class')
+else:
+    print('No Pass')
