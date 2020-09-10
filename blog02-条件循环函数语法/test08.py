c80b4d95711d60726efb43989a0aae2fd23eb7cb
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-09-10 19:00:27 +0800

    Add files via upload

---
+++ blog02-条件循环函数语法/test08.py
@@ -0,0 +1,14 @@
+#元组循环
+tup = (1,2,3,4,5)  
+for n in tup:  
+    print(n)  
+else:  
+    print('End for\n')
+#计算1+2+...+100
+s = 0
+for i in range(101):
+    s = s + i
+print('sum =', s)
+#输出三角形星号
+for i in range(10):
+    print("*"*i)
