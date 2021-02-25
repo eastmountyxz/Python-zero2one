97f1d616bc23e675bec22e8490f0eee5f6cd2e97
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-02-25 21:15:21 +0800

    Add files via upload

---
+++ blog07-Requests豆瓣电影/test05.py
@@ -0,0 +1,10 @@
+import requests
+
+data = {'some': 'data'}
+headers = {'content-type': 'application/json',
+           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+ 
+r = requests.post('https://api.github.com/some/endpoint',
+                  data=data,
+                  headers=headers)
+print(r.text)
