c839de490c8a133e7f024f7799eec12eebc14b6e
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-07-07 19:02:07 +0800

    Add files via upload

---
+++ blog14-classifier/blog14-classifier-04-dtc.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# By:Eastmount CSDN 2021-07-06
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_iris   
+from sklearn.tree import DecisionTreeClassifier 
+
+#载入鸢尾花数据集
+iris = load_iris()         
+X = X = iris.data[:, :2]   #获取花卉前两列数据
+Y = iris.target           
+lr = DecisionTreeClassifier()  
+lr.fit(X,Y)
+
+#meshgrid函数生成两个网格矩阵
+h = .02
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+#pcolormesh函数将xx,yy两个网格矩阵和对应的预测结果Z绘制在图片上
+Z = lr.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.figure(1, figsize=(8,6))
+plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+#绘制散点图
+plt.scatter(X[:50,0], X[:50,1], color='red',marker='o', label='setosa')
+plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker='x', label='versicolor')
+plt.scatter(X[100:,0], X[100:,1], color='green', marker='s', label='Virginica') 
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+plt.legend(loc=2) 
+plt.show()
