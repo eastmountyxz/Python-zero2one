fd97feeb50e77561d7aaf0ad26d501d7a4bf458b
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2022-02-18 16:14:10 +0800

    Create 36-1-YSadd.py

---
+++ blog36-opencv-logical-operations/36-1-YSadd.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+# By：Eastmount
+import cv2  
+import numpy as np  
+ 
+#读取图片
+img = cv2.imread("luo.png")
+
+#图像各像素加100
+m = np.ones(img.shape, dtype="uint8")*100
+
+#OpenCV加法运算
+result = cv2.add(img, m)
+
+#显示图像
+cv2.imshow("original", img)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
