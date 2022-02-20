7040fd05217bbe568f0f9e801f7d5295023e628c
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2022-02-20 18:40:55 +0800

    Create 37-1-AddImage.py

---
+++ blog37-opencv-roi/37-1-AddImage.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+# By：Eastmount
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图片
+src1 = cv2.imread('lena.png')
+src2 = cv2.imread('luo.png')
+
+#图像融合
+result = cv2.addWeighted(src1, 1, src2, 1, 0)
+
+#显示图像
+cv2.imshow("src1", src1)
+cv2.imshow("src2", src2)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
