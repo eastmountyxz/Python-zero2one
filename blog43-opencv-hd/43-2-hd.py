b8cc5307a267a52a7fb9427b7bc510c32dcd25eb
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2022-08-16 17:00:50 +0800

    Add files via upload

---
+++ blog43-opencv-hd/43-2-hd.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# By：Eastmount
+import cv2  
+import numpy as np  
+
+#读取原始图片
+src = cv2.imread('luo.png')
+
+#图像灰度化处理
+grayImage = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+
+#显示图像
+cv2.imshow("src", src)
+cv2.imshow("result", grayImage)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
