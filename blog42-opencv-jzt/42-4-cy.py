43db361b09c844d40470b62725e52d9bfd89e9c6
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2022-08-16 16:58:53 +0800

    Add files via upload

---
+++ blog42-opencv-jzt/42-4-cy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# By：Eastmount
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+
+#读取原始图像
+img = cv2.imread('school.png')
+
+#图像向下取样
+r1 = cv2.pyrDown(img)
+r2 = cv2.pyrDown(r1)
+r3 = cv2.pyrDown(r2)
+
+#显示图像
+cv2.imshow('original', img)
+cv2.imshow('PyrDown1', r1)
+cv2.imshow('PyrDown2', r2)
+cv2.imshow('PyrDown3', r3)
+cv2.waitKey()
+cv2.destroyAllWindows()
