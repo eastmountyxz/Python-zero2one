c0496fc1df9585bbaa30746d6e85b6092d075479
everfight <cbb903601682@163.com>
2023-06-13 16:53:14 +0800

    Update blog14-classifier-06-knn.py
    modify file path name

--- blog14-classifier/blog14-classifier-06-knn.py
+++ blog14-classifier/blog14-classifier-06-knn.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-  
 import os 
 import numpy as np
-path = "wine/wine.txt"
+path = "wine/wine Data Set Information.txt"
 data = np.loadtxt(path,dtype=float,delimiter=",")
 print(data)
 
