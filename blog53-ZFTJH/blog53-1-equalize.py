c38e96b915aa430da968394a647e1db2f8950eb0
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2023-09-14 12:37:34 +0800

    Create blog53-1-equalize.py

---
+++ blog53-ZFTJH/blog53-1-equalize.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# By：Eastmount
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图片
+img = cv2.imread('luo.png')
+
+#灰度转换
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+#直方图均衡化处理
+result = cv2.equalizeHist(gray)
+
+#显示图像
+cv2.imshow("Input", gray)
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
